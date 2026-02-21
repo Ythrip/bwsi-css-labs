b533beb8990369f3f8594484df50a1befe91ef0f
Ythrip <aronmir949@gmail.com>
2026-02-21 16:02:51 -0700

    Update lab_1b.py

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -50,8 +50,6 @@ def main():
     print(f"===== Simple Calculator =====")
 
     # Ask the user for sample input    
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
     num1 = request_sanetized_number("Enter the first number: ")
     num2 = request_sanetized_number("Enter the second number: ")
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
